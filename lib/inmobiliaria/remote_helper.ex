94b94b5ecd0b19968e6a1d65f118a38fc0b4b684
MisseAG <augusto.misseg@uqvirtual.edu.co>
2026-05-24 10:41:38 -0500

    Validación de error en Remote Helper

--- lib/inmobiliaria/remote_helper.ex
+++ lib/inmobiliaria/remote_helper.ex
@@ -1,7 +1,12 @@
 defmodule Inmobiliaria.RemoteHelper do
   def unir(nodo_servidor) do
-    Node.connect(nodo_servidor)
-    IO.puts("Conectado al servidor. Iniciando CLI...")
-    Inmobiliaria.CLI.start()
+    case Node.connect(nodo_servidor) do
+      true ->
+        IO.puts("Conectado al servidor. Iniciando CLI...")
+        Inmobiliaria.CLI.start()
+      false ->
+        IO.puts("Error: No se pudo conectar al nodo servidor '#{nodo_servidor}'")
+        {:error, "Conexión fallida"}
+    end
   end
 end
